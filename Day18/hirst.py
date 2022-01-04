e0b0ccc3b8de898bc0ee15abb5ee8c4bb8d52aae
DeepakKumarSingh <deepak.singh.buzz@gmail.com>
2022-01-04 17:26:34 +0100

    Hirst changes

--- Day18/hirst.py
+++ Day18/hirst.py
@@ -22,12 +22,9 @@ import random
 
 list_of_color = [(226, 231, 236), (58, 106, 148), (224, 200, 110), (134, 84, 58), (223, 138, 62), (196, 145, 171), (234, 226, 204), (224, 234, 230), (142, 178, 203), (139, 82, 105), (208, 90, 69), (237, 225, 233), (188, 80, 120), (69, 105, 90), (133, 182, 135), (133, 133, 74), (64, 156, 92), (47, 156, 193), (183, 192, 201), (213, 177, 191), (19, 58, 92), (20, 68, 113), (113, 123, 149), (227, 174, 166), (172, 203, 183), (156, 206, 217), (69, 58, 47), (72, 64, 53), (111, 46, 59), (53, 70, 64)]
 tim = t.Turtle()
-tim.shape("arrow")
+# tim.shape("arrow")
 t.colormode(255)
-
-tim.setheading(225)
-tim.forward(350)
-tim.setheading(0)
+tim.speed("fastest")
 
 
 def change_direction():
@@ -44,10 +41,30 @@ def draw_straight_dots():
         tim.fd(50)
 
 
-for j in range(11):
-    draw_straight_dots()
-    change_direction()
 
+# for j in range(11):
+#     draw_straight_dots()
+#     change_direction()
+
+def hirst_paint():
+    tim.penup()
+    tim.hideturtle()
+    tim.setheading(225)
+    tim.forward(350)
+    tim.setheading(0)
+    number_of_dots = 100
+    for dot_count in range(1 , number_of_dots +1):
+        tim.dot(20, random.choice(list_of_color))
+        tim.forward(50)
+        if dot_count % 10 == 0:
+            tim.setheading(90)
+            tim.forward(50)
+            tim.setheading(180)
+            tim.forward(500)
+            tim.setheading(0)
+
+
+hirst_paint()
 
 screen = t.Screen()
 screen.exitonclick()
